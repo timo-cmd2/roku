57f3a1f392e81f43da803808bb8f7effd18e868f
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-02-27 17:21:36 +0100

    Update roku.h

--- src/roku.h
+++ src/roku.h
@@ -1,4 +1,21 @@
-#ifndef ROKU_H
-#define ROKU_H
+#ifndef ROKU_H_INCL
+#define ROKU_H_INCL
+
+#ifdef __cplusplus 
+#extern "C" {
+#endif
+
+#include "value.h"
+#include "module.h"
+#include "thread.h"
+#include "globals.h"
+#include "errmsg.h"
+
+/** Global variable operations */
+R_APIFUNC RValue RGlobal(RThread *t, const char *name);
+
+#ifdef __cplusplpus
+}
+#endif
 
 #endif
