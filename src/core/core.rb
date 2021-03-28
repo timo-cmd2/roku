af34f33e2093cc77581731ad6117d717eb0d5552
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-28 19:19:15 +0200

    added initializing methods

--- src/core/core.rb
+++ src/core/core.rb
@@ -1,4 +1,9 @@
 module Core
   class Core
+    def initialize( debug_evaluator = false, debug_printer = false )
+      @debug_evaluator = debug_evaluator
+      @evaluator       = Evaluator.new( self, debug_evaluator )
+      @debug_printer   = debug_printer
+    end
   end
 end
