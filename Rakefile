92f9a37ecb85e987abbde9e6a2d5d8592b7d1876
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-18 08:04:19 +0200

    Added mixfile

--- Rakefile
+++
@@ -1,5 +0,0 @@
-# -*- mode: ruby; -*-
-# Rakefile for Roku
-require 'rake'
-require 'bundler/gem_tasks'
-require 'jeweler2'
---
+++ mix.exs
@@ -0,0 +1,12 @@
+defmodule Genshi.Mixfile do
+  use Mix.Project
+ 
+  def project do
+  end
+
+  def application do
+  end
+
+  defp deps do
+  end
+end
